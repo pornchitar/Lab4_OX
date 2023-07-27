d21032699bd9babc92d483e055871d2b3641f03b
Pornchitar Sarsai <kream21574@gmail.com>
2023-07-27 17:36:22 +0700

    Table

---
+++ src/main/java/com/pornchitar/lab4_ox/Lab4_OX.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.pornchitar.lab4_ox;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Lab4_OX {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
